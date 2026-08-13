ef02c7e45eed215a34455dd64da50ecc4e748c3e
Vlados8 <70662816+Vlados8@users.noreply.github.com>
2026-08-13 18:30:29 +0300

    Add Google Tag Manager (GTM-5QTHVKC3)

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,14 @@ export default function RootLayout({
   return (
     <html lang="de" className={inter.variable}>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5QTHVKC3');`}
+        </Script>
         {/* Google Analytics / Google Ads (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-18093667327"
@@ -50,6 +58,15 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="font-sans antialiased overflow-x-hidden">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QTHVKC3"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <Analytics />
       </body>
