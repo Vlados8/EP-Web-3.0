d351b5054b5fcd34ab631dae1a71a61716c611a0
Vlados8 <70662816+Vlados8@users.noreply.github.com>
2026-09-14 20:08:03 +0300

    feat: add Hotjar tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             oaiq("init", { pixelId: "UZIFDGTZHJZUKL" });
           `}
         </Script>
+        {/* Hotjar Tracking Code */}
+        <Script id="hotjar-tracking" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6777688,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
       </head>
       <body className="font-sans antialiased overflow-x-hidden">
         {/* Google Tag Manager (noscript) */}
